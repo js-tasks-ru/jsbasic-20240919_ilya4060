dba0477fe5b7dd915b2740f10c5816c721effe24
Данейкин Илья Вячеславович <Daneykin.Ilya@luis.ru>
2024-09-29 19:43:08 +0300

    написал функцию калькулятор для сложения и умножения двух чисел

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
-let calculator = {
-  // ваш код
+function Calculator() {
+  this.read = function(a, b) {
+    this.a = a;
+    this.b = b;
+  };
+
+  this.sum = function() {
+    return this.a + this.b;
+  };
+
+  this.mul = function() {
+    return this.a * this.b;
+  };
 };
+
+let calculator = new Calculator();
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
