d96f0d3d4ce74e25af5bf32f256ccaefce2e9e9d
Данейкин Илья Вячеславович <Daneykin.Ilya@luis.ru>
2024-10-08 18:22:10 +0300

    написал функцию для изменения стиля таблицы

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+
+let rows = table.querySelectorAll('tbody tr');
+rows.forEach(row => {
+  let ageCell = row.cells[1];
+  let genderCell = row.cells[2];
+  let statusCell = row.cells[3];
+  //Status
+  if (statusCell.hasAttribute('data-available')) {
+    let available = statusCell.getAttribute('data-available');
+    if (available === 'true') {
+      row.classList.add('available');
+    } else if (available === 'false') {
+      row.classList.add('unavailable');
+    }
+  } else {
+    row.hidden = true;
+  }
+  //Gender
+  if (genderCell.textContent === 'm') {
+    row.classList.add('male');
+  } else if (genderCell.textContent === 'f') {
+    row.classList.add('female');
+  }
+  //Age
+  if (parseInt(ageCell.textContent) < 18) {
+    row.style.textDecoration = 'line-through';
+  }
+});
 }
