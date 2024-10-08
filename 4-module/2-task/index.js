fb68c09a383263876a188e5f86d183c761085c30
Данейкин Илья Вячеславович <Daneykin.Ilya@luis.ru>
2024-10-08 12:36:41 +0300

    внес требуемую правку в 4-2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,4 @@
 function makeDiagonalRed(table) {
-  let table = document.querySelector('table');
-
   for (let i = 0; i < table.rows.length; i++) {
     let cell = table.rows[i].cells[i];
     cell.style.backgroundColor = 'red';
