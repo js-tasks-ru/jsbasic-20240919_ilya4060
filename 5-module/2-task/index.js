e588e08764758231943976c3794c8908dcfd8ddc
Данейкин Илья Вячеславович <Daneykin.Ilya@luis.ru>
2024-10-14 18:05:47 +0300

    написал функцию для скрытия элемента по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let div = document.getElementById('text');
+
+  function handler () {
+    if (div.hidden === false) {
+      div.hidden = true;
+    } else {
+      div.hidden = false;
+    }
+  };
+
+  button.addEventListener("click", handler);
 }
